be5f74bb969ed267ace619d2a0f5dd96a9610208
Anna Monakhova <anna.mon99@yandex.ru>
2021-09-08 12:33:25 +0300

    improved bottom-menu

--- js/index.js
+++ js/index.js
@@ -1,43 +1,25 @@
 // Header-menu
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.header__list-name-realism').addEventListener('click', function() {
-    document.querySelector('.header__bottom-block-realism').classList.toggle('is-active'),
-    document.querySelector('.header__list-name-realism').classList.toggle('is-active')
-  })
-});
+let link = document.querySelectorAll('.header__botoom-nav-item')
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.header__list-name-impressionism').addEventListener('click', function() {
-    document.querySelector('.header__bottom-block-impressionism').classList.toggle('is-active'),
-    document.querySelector('.header__list-name-impressionism').classList.toggle('is-active')
-  })
-});
+for (let i = 0; i < link.length; i++) {
+  link[i].addEventListener('click', activeLink);
+}
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.header__list-name-post-impressionism').addEventListener('click', function() {
-    document.querySelector('.header__bottom-block-post-impressionism').classList.toggle('is-active'),
-    document.querySelector('.header__list-name-post-impressionism').classList.toggle('is-active')
-  })
-});
+function activeLink(event) {
+  event.currentTarget.classList.toggle('is-active');
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.header__list-name-avant-garde').addEventListener('click', function() {
-    document.querySelector('.header__bottom-block-avant-garde').classList.toggle('is-active'),
-    document.querySelector('.header__list-name-avant-garde').classList.toggle('is-active')
-  })
-});
+  for (let j = 0; j < link.length; j++) {
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.header__list-name-futurism').addEventListener('click', function() {
-    document.querySelector('.header__bottom-block-futurism').classList.toggle('is-active'),
-    document.querySelector('.header__list-name-futurism').classList.toggle('is-active')
-  })
-});
+    if(link[j] !== event.currentTarget) {
+      link[j].classList.remove('is-active');
+    }
+  }
+}
 
 // Hero-swiper
 
-const swiper = new Swiper('.section-hero', {
+const swiper = new Swiper('.section-hero__swiper-container', {
   loop: true,
   autoplay: {
     delay: 5000,
